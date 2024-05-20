48601603fe214e855755cd674eea1a106592275d
JoaoVlltor <novoaff@gmail.com>
2024-05-20 19:44:57 -0300

    20/05

--- PassaNada/frmMoradores.vb
+++ PassaNada/frmMoradores.vb
@@ -1,13 +1,3 @@
-﻿Public Class frmMoradores
-    Private Sub NovoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnNovo.Click
+﻿Public Class Form3
 
-    End Sub
-
-    Private Sub lblCep_Click(sender As Object, e As EventArgs) Handles lblCep.Click
-
-    End Sub
-
-    Private Sub MaskedTextBox2_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles txtBuscar.MaskInputRejected
-
-    End Sub
 End Class
--- PassaNada/frmTelainicial.vb
+++ PassaNada/frmTelainicial.vb
@@ -1,13 +1,3 @@
-﻿Public Class frmTelainicial
-    Private Sub BtnVisitantes_Click(sender As Object, e As EventArgs) Handles BtnVisitantes.Click
+﻿Public Class Form2
 
-    End Sub
-
-    Private Sub HistóricoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HistóricoToolStripMenuItem.Click
-
-    End Sub
-
-    Private Sub FecharToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FecharToolStripMenuItem.Click
-
-    End Sub
 End Class
